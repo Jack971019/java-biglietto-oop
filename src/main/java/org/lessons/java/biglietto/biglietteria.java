61788ca7e44e9b6aeb7a38c0324892df7c780368
Giacomotestori <giacomo.testori10@gmail.com>
2023-09-04 09:47:57 +0200

    created classes bigliettera and biglietto

---
+++ src/main/java/org/lessons/java/biglietto/biglietteria.java
@@ -0,0 +1,7 @@
+package org.lessons.java.biglietto;
+
+public class biglietteria {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/lessons/java/biglietto/biglietto.java
@@ -0,0 +1,4 @@
+package org.lessons.java.biglietto;
+
+public class biglietto {
+}
