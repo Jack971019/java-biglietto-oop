be6903e4b5405dd8bc175d496210ae793f9a21eb
Giacomotestori <giacomo.testori10@gmail.com>
2023-09-04 11:25:35 +0200

    added ticket price calculating method

--- src/main/java/org/lessons/java/biglietto/biglietto.java
+++ src/main/java/org/lessons/java/biglietto/biglietto.java
@@ -19,7 +19,13 @@ public class biglietto {
     // COSTRUTTORI
 
     public biglietto(int km, int passengerAge) {
+        if (km < 0){
+            throw new RuntimeException();
+        }
         this.km = km;
+       if (passengerAge < 0){
+           throw new RuntimeException();
+       }
         this.passengerAge = passengerAge;
     }
 
@@ -27,23 +33,56 @@ public class biglietto {
     // GETTER AND SETTER
 
     public int getKm() {
+        if (km < 0){
+            throw new RuntimeException();
+        }
         return km;
     }
 
     public void setKm(int km) {
+        if (km < 0){
+            throw new RuntimeException();
+        }
         this.km = km;
     }
 
     public int getPassengerAge() {
+        if (passengerAge < 0){
+            throw new RuntimeException();
+        }
         return passengerAge;
     }
 
     public void setPassengerAge(int passengerAge) {
+        if (passengerAge < 0){
+            throw new RuntimeException();
+        }
         this.passengerAge = passengerAge;
     }
 
 
     // METODI
+
+    private void validkm(int km){
+        if(km < 0) {
+            throw new RuntimeException();
+        }
+    }
+
+
+    // calcolare prezzo finale con lo sconto
+
+    private BigDecimal prezzoFinale(){
+        BigDecimal prezzo = BigDecimal.ZERO;
+        if(passengerAge > 65){
+            prezzo = costoPerChilometro.multiply(new BigDecimal(km)).multiply(scontoOver65);
+        } else if(passengerAge < 18){
+            prezzo = costoPerChilometro.multiply(new BigDecimal(km)).multiply(scontoUnder18);
+        } else {
+            prezzo = costoPerChilometro.multiply(new BigDecimal(km));
+        }
+        return prezzo;
+    }
 
 
 }
