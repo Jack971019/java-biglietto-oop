c7ff9724d0f304c92792bd41e479c6e05a0cccc2
Giacomotestori <giacomo.testori10@gmail.com>
2023-09-04 10:28:06 +0200

    added costanti and getter and setter

--- src/main/java/org/lessons/java/biglietto/biglietto.java
+++ src/main/java/org/lessons/java/biglietto/biglietto.java
@@ -1,4 +1,49 @@
 package org.lessons.java.biglietto;
 
+import java.math.BigDecimal;
+
 public class biglietto {
+
+    // ATTRIBUTI
+
+    private int km;
+
+    private int passengerAge;
+
+    final BigDecimal costoPerChilometro = new BigDecimal("0,21");
+    final BigDecimal scontoOver65 = new BigDecimal("0,40");
+    final BigDecimal scontoUnder18 = new BigDecimal("0,20");
+
+
+
+    // COSTRUTTORI
+
+    public biglietto(int km, int passengerAge) {
+        this.km = km;
+        this.passengerAge = passengerAge;
+    }
+
+
+    // GETTER AND SETTER
+
+    public int getKm() {
+        return km;
+    }
+
+    public void setKm(int km) {
+        this.km = km;
+    }
+
+    public int getPassengerAge() {
+        return passengerAge;
+    }
+
+    public void setPassengerAge(int passengerAge) {
+        this.passengerAge = passengerAge;
+    }
+
+
+    // METODI
+
+
 }
